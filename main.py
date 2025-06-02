cf39d0e2e76acd61a8a201b61d5cc69fb1b13207
anıl <09abab09987890@gmail.com>
2025-06-02 17:45:06 +0300

    İlk commit: Flask mesajlaşma uygulaması

--- main.py
+++ main.py
@@ -159,4 +159,4 @@ def get_messages():
     return jsonify(conversation)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
